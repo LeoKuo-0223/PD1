24eddb0b58f018f81a6efe585bcfbc7a17aea106
LeoKuo-0223 <leo900223@gmail.com>
2021-12-07 23:53:17 +0800

    finish lonely num for lec9_2

--- HW9_2/lonelyNum.c
+++ HW9_2/lonelyNum.c
@@ -1,117 +1,103 @@
-#include<stdio.h>
+#include <stdio.h>
+#include<stdbool.h>
 
-int main () {
-    int n;
+int n;
+
+bool finish=false;
+void quicksort(int a[], int low, int high);
+int split(int a[], int low, int high);
+void lonely(int array[],int remain);
+bool check(int array[], int i,int j,int a);
+int main(){
+    int lonelyNumIndex;
     scanf("%d",&n);
-    int array[n];
-    int record[n];
-    int record_B[n];
-    int record_C[n];
+    int lonelyarray[n];
     for(int i=0;i<n;i++){
-        record[i] = 0;
-        record_B[i] = 0;
-        record_C[i] = 0;
-        scanf("%d",&array[i]);
+        scanf("%d",&lonelyarray[i]);
     }
-    int lonelyNumIndex=0;
-    int b=1;
-    int c=2;
-    int next=0;
-    record_B[b]=1;
-    record_C[b]=1;
-    record_B[lonelyNumIndex]=1;
-    record_C[lonelyNumIndex]=1;
-    for(;;){
-        if(next==1){
-            c=0;
+    quicksort(lonelyarray,0,n-1);
+    for(int i=0;i<n;i++){
+        if(finish==false){
+            lonelyNumIndex=i;
+            int temparray[n-1];
+            int count=0;
             for(int j=0;j<n;j++){
-                record_C[j] = 0;
-                if(record[j]==1) record_C[j] = 1;
-            }
-            for(;;){
-                if(record[b]==1||record_B[b]==1){
-                    b++;
-                    if(b==n){
-                        printf("%d",array[lonelyNumIndex]);
-                        return 0;
-                    }
-                    
-                }else {
-                    record_B[b]=1;
-                    record_C[b]=1;
-                    break;
+                if(j!=lonelyNumIndex){
+                    temparray[count]=lonelyarray[j];
+                    count++;
                 }
             }
-
-            next=0;
-        }
-    
-        for(;;){
-            if(record[c]==1||record_C[c]==1){
-                c++;
-                if(c==n) c=c%n;
-            }else break;
-            
-        }
-
-
-        int sum = array[lonelyNumIndex] + array[b];
-        if(sum==array[c]){
-            record[lonelyNumIndex] = 1;
-            record[b] = 1;
-            record[c] = 1;
-            for(int j=0;j<n;j++){
-                record_B[j] = 0;
-                record_C[j] = 0;
-            }
-            for(int i=0;i<n;i++){
-                if(record[i]==1){
-                    record_B[i] = 1;
-                    record_C[i] = 1;
-                }
-            }
-            for(;;){
-                if(record[lonelyNumIndex]==1){
-                    lonelyNumIndex++;
-                    if(lonelyNumIndex==n-1) {
-                        printf("%d",array[lonelyNumIndex]);
-                        return 0;
-                    }
-
-                }else {
-                    next=1;
-                    record_C[lonelyNumIndex] = 1;
-                    record_B[lonelyNumIndex] = 1;
-                    break;
-                }
-            
-            }
-        }else{
-            record_C[c] = 1;
-            for(int i=0;i<n;i++){
-                if(record_C[i]==0){
-                    next=0;
-                    break;
-                }
-                next=1;
-            }
-        }
-
-
+            lonely(temparray,n-1);
+        }else break;
     }
-    return 0;
+    printf("%d",lonelyarray[lonelyNumIndex]);
+
 }
 
+void lonely(int array[],int remain){
+    if(remain==0){
+        finish=true;
+        return;
+    }else{
+        int a=remain-1;
+        int previousI=0;
+        int previousJ=0;
+        for(int i=0;i<remain-1;i++){
+            if(array[i]!=previousI){
+                previousI=array[i];
+                previousJ=0;
+                for(int j=i+1;j<remain-1;j++){
+                    if(array[j]!=previousJ){
+                        previousJ=array[j];
+                        bool answer = check(array,i,j,a);
+                        if(answer == true){
+                            int temparray[remain-3];
+                            int count=0;
+                            for(int k=0;k<remain;k++){
+                                if(k!=i && k!=j){
+                                    temparray[count]=array[k];
+                                    count++;
+                                }
+                            }
+                            lonely(temparray,remain-3);
+                        }
+                    }
 
-// record_B[b]=1;
+                }
+            }
 
-//     for(;;){
-//             if(b==lonelyNumIndex ||record[b]==1||record_B[b]==1){
-//                 b++;
-//                 if(b==n){
-//                     printf("%d",array[lonelyNumIndex]);
-//                     return 0;
-//                 }
-                
-//             }else break;
-//         }
+        }
+    }
+}
+
+bool check(int array[], int i,int j,int a){
+    int sum=(array[i]+array[j]);
+    if(sum==array[a]) {
+        return true;
+    }else return false;
+}
+
+void quicksort(int a[], int low,int high){
+int middle;
+if (low >= high) return;
+middle = split(a, low, high);
+quicksort(a, low, middle - 1);
+quicksort(a, middle + 1, high);
+}
+
+int split(int a[], int low, int high)
+{
+int part_element = a[low];
+for (;;) {
+while (low < high && part_element <= a[high])
+high--;
+if (low >= high) break;
+a[low++] = a[high];
+while (low < high && a[low] <= part_element)
+low++;
+if (low >= high) break;
+a[high--] = a[low];
+}
+a[high] = part_element;
+return high;
+}
